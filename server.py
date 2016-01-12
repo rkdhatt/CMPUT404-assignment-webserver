44bc1d4824b5e393dc53ea8325ac251f671abbdc
rkdhatt <rkdhatt@ualberta.ca>
2016-01-11 18:40:02 -0700

    Added name to licence

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import SocketServer
 import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Raman Dhatt, Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
